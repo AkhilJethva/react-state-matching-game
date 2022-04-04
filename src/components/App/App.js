346eee25593cf1f2ca31939c6516710bfaa5fe1c
Akhil jethva <68720806+AkhilJethva@users.noreply.github.com>
2022-04-04 10:41:20 +0530

    "First One"

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,15 +6,32 @@ import './App.css';
 
 class App extends Component{
 
+  constructor(props){
+    super(props);
+    this.state = {
+      numTiles: 36,
+      playing: false,
+      previousTileIndex:null,
+      tiles: [],
+      toBeCleared:null
+    };
+  }
+
+  startGame(numTiles){
+    (state)=>(
+      this.setState({playing: true, previousTileIndex:null,toBeCleared:null})
+    )
+  }
+
   render() {
   return (
     <div className="App">
       <header className="App-header">
         Turbo-Matcher
       </header>
-        <OptionsPanel />
-        <Board />
-      }
+        <OptionsPanel playing={this.state.playing} numTiles={this.state.numTiles}/>
+        <Board numTiles={this.state.numTiles} tiles={this.state.tiles}/>
+      
     </div>
   );
 
